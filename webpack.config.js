aeded15899937790915125d4cb0ba74740f8fb6d
Nathaniel Foster <nathanielsmusic@gmail.com>
2017-12-15 15:57:44 -0700

    fix index.html bundle location, add .jsx to webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
